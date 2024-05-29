9e67d11c74dd7b97f2d12066d68ade1868dc5f46
Francesco Scutellaro <dev.mirror42@gmail.com>
2024-05-29 21:55:17 +0200

    feat: add eslint

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
     testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
+    setupFiles: ["<rootDir>/jest.setup.js"],
 };
--- src/indes.ts
+++ src/indes.ts
@@ -1,4 +1,5 @@
 import app from "./app";
+import "dotenv/config";
 
 const port = process.env.PORT || 3000;
 
