6891afa4fce089ced4896d5d6b58d25704d5f059
Francesco Scutellaro <dev.mirror42@gmail.com>
2024-05-30 19:34:20 +0200

    feat: add express router

--- src/routes/errorRoute.ts
+++ src/routes/errorRoute.ts
@@ -2,8 +2,6 @@ import { ErrorRequestHandler } from "express";
 
 // TODO: use express router
 export const errorRoute: ErrorRequestHandler = (err, _req, res, _next) => {
-    console.error("fffffffffffffffffffffffffffffff: ", err);
-
     // if CORS error, return 403
     if (err.message === "Not allowed by CORS") {
         res.setHeader("Content-Type", "text/plain; charset=utf-8");
